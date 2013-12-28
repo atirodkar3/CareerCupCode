e64943ebc2da4f7ab65f8f0144ade3ceab8d8f12
Man Ray <manray@Mans-MacBook-Pro.local>
2013-12-28 18:17:37 +0100

    Can Run Iterations on Knights Tour with Random Moves

--- KnightsTour.java
+++ KnightsTour.java
@@ -1,9 +1,8 @@
+import java.util.LinkedList;
+
 public class KnightsTour {
     public static void main(String[] args) {
-        Table table = new Table(8);
-        table.setTable(4,4,1);
-        table.setTable(18,2,1);
-        table.displayTable();
+        Knight k = new Knight(8);
     }
 }
 
@@ -15,13 +14,19 @@ class Table {
         table = new int[length][length];
     }
 
-    public void setTable(int i, int j,int value) {
+    public void setTable(Position p ,int value) {
+        int i = p.getX();
+        int j = p.getY(); 
         if(i - 1 > table.length || i - 1 < 0 || j - 1 > table.length || j - 1 < 0) {
             System.out.println("Position " + i + " " + j + " Invalid");
         } else {
             table[i - 1][j - 1] = value;
-            System.out.println("Position " + i + " " + j + " Set");
+            //System.out.println("Position " + i + " " + j + " Set");
         }
+    }
+
+    public int getTableValue(Position p) {
+        return (table[p.getX() - 1][p.getY() - 1]);
     }
 
     public void displayTable() {
@@ -32,5 +37,109 @@ class Table {
             }
         }
         System.out.println();
+    }
+
+    public int getTableLength() {
+        return length;
+    }
+}
+
+class Position {
+    int x;
+    int y;
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x; 
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+class Knight {
+    private Position startPos;
+    public Knight(int length) {
+        Table bestTable = null;
+        int bestVal = 0;        
+        for(int testNum = 0; testNum < 10000001; testNum++) {
+            Table table = new Table(length);
+            int startX = 1 + (int)(Math.random() * ((length) - 1));
+            int startY = 1 + (int)(Math.random() * ((length) - 1));
+            startPos = new Position(startX, startY);
+            int iterVal = iterateTour(table, startPos);
+            if(iterVal > bestVal) {
+                table.displayTable();
+                bestTable = table;
+                bestVal = iterVal;
+            }
+        }
+        System.out.println(bestVal);
+    }
+
+    public int iterateTour(Table table, Position startPos) {
+        Position newP = startPos;
+        int iteration = 1;
+        //System.out.println(newP.getX() + " " + newP.getY());
+        while(newP != null) {
+            table.setTable(newP, iteration++);
+            //System.out.println("Start Position Set at " + newP.getX() + " " + newP.getY());
+            newP = getNextPosition(newP, table);
+        }
+        return iteration;   
+    }
+
+    private Position getNextPosition(Position currentPosition, Table table) {
+        int x = currentPosition.getX();
+        int y = currentPosition.getY();
+
+        LinkedList<Position> possiblePositions = getListOfPossiblePositions(x, y, table);        
+        return decidePosition(possiblePositions);
+    }
+
+    private Position decidePosition(LinkedList<Position> openPositions) {
+        // Define Algorithm
+
+        if(openPositions.size() == 0) {
+            return null;
+        } else {
+            int pos = (int)(Math.random() * (openPositions.size()));
+            return openPositions.get(pos);
+        }
+    }
+
+    private LinkedList<Position> getListOfPossiblePositions(int x, int y, Table table) {
+        int length = table.getTableLength();
+        LinkedList<Position> openSpots = new LinkedList<Position>();
+        openSpots.add(new Position(x - 2, y + 1));
+        openSpots.add(new Position(x - 2, y - 1));
+        openSpots.add(new Position(x + 2, y + 1));
+        openSpots.add(new Position(x + 2, y - 1));
+        openSpots.add(new Position(x + 1, y + 2));
+        openSpots.add(new Position(x + 1, y - 2));
+        openSpots.add(new Position(x - 1, y + 2));
+        openSpots.add(new Position(x - 1, y - 2));
+        
+        int iterator = 0;
+        while(iterator < openSpots.size()) {
+            Position posIteration = openSpots.get(iterator);
+            int posX = posIteration.getX();
+            int posY = posIteration.getY();
+            
+            if(posX < 1 || posX > length || posY < 1 || posY > length) {
+                openSpots.remove(iterator);
+            } else { 
+                if (table.getTableValue(posIteration) != 0) {
+                    openSpots.remove(iterator);  
+                } else {
+                    iterator++;
+                }
+            }
+        }
+        return openSpots;
     }
 }
